fe854d9f2bc77d946c133c260e2d6b0cf534ea65
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 16:07:32 -0600

    Added code and comments

---
+++ AssignmentDemos.py
@@ -0,0 +1,11 @@
+# Sequence unpacking = Allows you to take object in a collection and store them in variables for later use
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+# Switching values
+x = 1 
+y = 2
+
+x, y = y, x
+
+print(x, y)
