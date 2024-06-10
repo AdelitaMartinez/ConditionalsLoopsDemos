3fce6a4f97990b88e6b1cd94f641d4f543331143
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 16:46:13 -0600

    Added  more notes

---
+++ AugmentedAssignmentsDemo.py
@@ -0,0 +1,14 @@
+# Augmented assignments with addition
+x = 2
+x += 1
+print(x)
+
+# Can also do this with minus
+x = 2
+x -= 1
+print(x)
+
+# Can also be done with multiplication and division
+x = 2
+x *= 1
+print(x)
