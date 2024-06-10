cd9f2940944a2c54d8e4cf0d190480eef302f5f7
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 16:10:59 -0600

    Added comments to updated code

--- AssignmentDemos.py
+++ AssignmentDemos.py
@@ -5,7 +5,15 @@ print(x, y, z)
 # Switching values
 x = 1 
 y = 2
-
 x, y = y, x
-
 print(x, y)
+
+# Works with variables that are sequences too
+# Values below = tuples (Collection of objects seperated by commas. Similar to a list in terms of indexing, 
+# nested objects, and repitition, BUT a tuple is immutable (cannot be changed/modified))
+values = 1,2,3
+print(values)
+
+x,y,z = values
+
+print(x,y,z)
