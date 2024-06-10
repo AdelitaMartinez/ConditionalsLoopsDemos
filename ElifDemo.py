a499be38e0c92e21e8d5819c00103047636a7679
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 17:16:23 -0600

    Added code to file

---
+++ ElifDemo.py
@@ -0,0 +1,10 @@
+# Get a number from user
+num = float(input("Please enter a number: "))
+
+# Report positive if number is positive 
+if num > 0:
+  print('Positive')
+elif num < 0: # Report negative if number is negative
+  print('Negative')
+else:  # Report zero otherwise
+  print('Zero')
