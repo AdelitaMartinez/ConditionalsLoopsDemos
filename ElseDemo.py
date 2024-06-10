76d23bbb3220200f658924d3ee40fe66289afbcc
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 17:11:11 -0600

    Added code

---
+++ ElseDemo.py
@@ -0,0 +1,6 @@
+name = input('Please enter a name: ')
+if name == 'Rob':
+  print('Welcome back Rob!')
+else:
+  print("Welcome new person I don't know!")
+print("Thank you for using my program.")
---
+++ NumericalElseDemo.py
@@ -0,0 +1,14 @@
+# Header (always executes)
+print("Welcome to super odd/even calc 3000!")
+
+# Get a number from user
+num = float(input("Please enter a number: "))
+
+# If odd say off
+if num%2 == 1:
+  print("Odd")
+else: # Otherwise say even
+  print("Even")
+
+# Say goodbye (always execute)
+print('Thank you for using my program.')
