56769c9fd2a6fe4683cfccf5d1b669d7001a8eab
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 17:02:51 -0600

    Finished this file

--- IfDemo.py
+++ IfDemo.py
@@ -1,3 +1,5 @@
-text = 'hello'
+text = input('Please enter a statement: ')
+# Notice colon and indention, these are needed
 if text == 'hello':
   print('You said hello!')
+print('Thank you for using my program.')
