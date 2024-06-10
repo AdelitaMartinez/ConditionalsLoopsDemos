5c9ef67cd074950ffa26735be8073b714ba22d8d
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 17:22:11 -0600

    Added more code

---
+++ ElifOrderDemo.py
@@ -0,0 +1,13 @@
+# Get number from user
+num = float(input('Please enter a number: '))
+
+# If number > 30 say greater than 30
+if num > 30:
+  print('Greater than 30')
+
+# If number > 20 say greater than 20
+elif num > 20:
+  print('Greater than 20')
+  # If number > 10 say greater than 10
+elif num > 10: 
+  print('Greater than 10')
