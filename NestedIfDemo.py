8b80bed417558b8841ecf58e3ba5acf1d015f5ff
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 18:43:42 -0600

    Added file

---
+++ NestedIfDemo.py
@@ -0,0 +1,16 @@
+# Display header
+print("Hello welcome to my program!")
+
+# Get input from user
+user_input = input("Please enter a number: ")
+# If input is numeric
+print(user_input.isnumeric())
+  # if positive say positive
+
+  # if negative say negative
+  
+  # else say zero 
+
+# else say not numeric
+
+# Goodbye message
