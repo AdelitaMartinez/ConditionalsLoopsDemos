242223d946bdb12cb4de94a6bacd08b8574470cb
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 18:35:30 -0600

    Added pseudocode

--- IfDemo.py
+++ IfDemo.py
@@ -1,5 +1,10 @@
+# Get text from user
 text = input('Please enter a statement: ')
+
+# If text is hello, say you said hello
 # Notice colon and indention, these are needed
 if text == 'hello':
   print('You said hello!')
+
+  # Print thank you 
 print('Thank you for using my program.')
