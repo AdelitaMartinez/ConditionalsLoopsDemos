20a7a046d6ba9cfe5abe985ffbe44ac63eecd005
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 18:54:02 -0600

    Added more logic to code

--- NestedIfDemo.py
+++ NestedIfDemo.py
@@ -4,13 +4,23 @@ print("Hello welcome to my program!")
 # Get input from user
 user_input = input("Please enter a number: ")
 # If input is numeric
-print(user_input.isnumeric())
-  # if positive say positive
 
-  # if negative say negative
-  
-  # else say zero 
+
+# has only one .
+if user_input.count('.') <= 1:
+
+  user_input_without_pt = user_input.replace('.', '')
+  print(user_input_without_pt)
+  print(user_input.isnumeric())
+  # all remaining are numeric
+    # if positive say positive
+
+    # if negative say negative
+    
+    # else say zero 
 
 # else say not numeric
+else:
+  print('Not numeric')
 
 # Goodbye message
