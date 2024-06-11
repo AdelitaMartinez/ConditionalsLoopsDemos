a53b1713c9e5a00c497e0dffdd72624c5c12f576
Adelita Martinez <amartinez1013@cnm.edu>
2024-06-10 19:03:53 -0600

    That's enough for today

--- NestedIfDemo.py
+++ NestedIfDemo.py
@@ -11,14 +11,16 @@ if user_input.count('.') <= 1:
 
   user_input_without_pt = user_input.replace('.', '')
   print(user_input_without_pt)
-  print(user_input.isnumeric())
+  if user_input.isnumeric():
   # all remaining are numeric
+    print("Is numeric!")
     # if positive say positive
-
+    
     # if negative say negative
     
     # else say zero 
-
+  else:
+    print('Not numeric')
 # else say not numeric
 else:
   print('Not numeric')
